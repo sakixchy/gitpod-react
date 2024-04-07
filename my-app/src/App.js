77ca0948577679c83656939f663c0538f905864a
Saki Chy <sakibchowdhury79@gmail.com>
2024-04-07 10:56:01 +0000

    create react app with components

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;
